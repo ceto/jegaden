b20a6020ca322e83f3a6c167f4506f467018ccdb
Gabor Szabo <szabogabi@gmail.com>
2019-07-09 14:44:56 +0200

    second commit

--- lib/custom.php
+++ lib/custom.php
@@ -1,5 +1,23 @@
 <?php
+    // 1. customize ACF path
+    add_filter('acf/settings/path', 'jegaden_acf_settings_path');
+    function jegaden_acf_settings_path( $path ) {
+        $path = get_stylesheet_directory() . '/lib/acf/';
+        return $path;
+    }
 
+    // 2. customize ACF dir
+    add_filter('acf/settings/dir', 'jegaden_acf_settings_dir');
+    function jegaden_acf_settings_dir( $dir ) {
+    $dir = get_stylesheet_directory_uri() . '/lib/acf/';
+    return $dir;
+    }
 
+    // 3. Hide ACF field group menu item
+    //add_filter('acf/settings/show_admin', '__return_false');
 
+    // 4. Include ACF
+    include_once( get_stylesheet_directory() . '/lib/acf/acf.php' );
 
+    // 5. Unhide native metabox
+    add_filter('acf/settings/remove_wp_meta_box', '__return_false');
