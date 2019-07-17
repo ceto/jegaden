9e73dc98be93b6ad1bc1f3b48e7841e9a7e46170
Szabó Gábor <szabogabi@gmail.com>
2019-07-17 13:23:39 +0200

    ok

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/hero'); ?>
-  <?php get_template_part('templates/features'); ?>
-  <?php get_template_part('templates/testimonials'); ?>
-  <?php get_template_part('templates/servicelist'); ?>
-  <?php get_template_part('templates/patientresources'); ?>
-  <?php get_template_part('templates/placeswherework'); ?>
-  <?php //get_template_part('templates/content', 'page'); ?>
+<?php get_template_part('templates/hero'); ?>
+<?php get_template_part('templates/features'); ?>
+<?php get_template_part('templates/testimonials'); ?>
+<?php get_template_part('templates/servicelist'); ?>
+<?php get_template_part('templates/patientresources'); ?>
+<?php get_template_part('templates/placeswherework'); ?>
+<?php get_template_part('templates/logos'); ?>
+<?php //get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
---
+++ templates/logos.php
@@ -0,0 +1,113 @@
+<div class="grid-container ps">
+    <div class="grid-x">
+        <div class="cell">
+            <div class="grid-x grid-margin-x small-up-3 large-up-5 align-center">
+                <?php $i=0; ?>
+                <?php // while ( have_rows('cl_logos', 'options') ) : the_row(); ?>
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/aats.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/ccad.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/ctsnet.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/cwru.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/EACTS.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/logo_hcl.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/mediclinic.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/sts.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell">
+                    <figure class="clientlogo">
+                        <?php
+                                // $logoimage = get_sub_field('logo');
+                                // echo wp_get_attachment_image($logoimage['id'], 'medium' );
+                        ?>
+                        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/logos/uclb.jpg" alt="">
+                        <figcaption><?php // the_sub_field('clientsname') ?></figcaption>
+                    </figure>
+                </div>
+
+
+
+
+                <?php //endwhile; ?>
+            </div>
+        </div>
+    </div>
+</div>
