059ecb7bf84bd6f89439be2eb7dd18e0f005cda4
Gabor Szabo <szabogabi@gmail.com>
2019-07-18 14:36:14 +0200

    bemut 2

--- templates/patientresources.php
+++ templates/patientresources.php
@@ -1,6 +1,7 @@
 <section class="patientresources ps ps--light ps--bordered">
     <div class="grid-container">
         <h2>Patient Resources</h2>
+        <br>
 
 
         <ul class="resourcelist">
@@ -8,14 +9,14 @@
                 <div class="rescard">
                     <h3 class="rescard__title">Post Surgery Instructions</h3>
                     <p class="rescard__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, iure deleniti? Enim!</p>
-                    <a href="#" class="rescard__more">Read More</a>
+                    <a href="#" class="rescard__more button">Read More</a>
                 </div>
             </li>
             <li>
                 <div class="rescard">
                     <h3 class="rescard__title">Evaulation and Surgery</h3>
                     <p class="rescard__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloremque possimus quod maxime!</p>
-                    <a href="#" class="rescard__more">Read More</a>
+                    <a href="#" class="rescard__more button">Read More</a>
                 </div>
             </li>
         </ul>
