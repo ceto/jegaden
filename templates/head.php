a5f9a3c275b6bbc779010532b50f8d6d7c9b740e
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2020-09-23 01:14:08 +0200

    rtl css support

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php if(ICL_LANGUAGE_CODE=='ar') {  ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/rtl.css" ?>" type="text/css" >
+<?php } ?>
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
