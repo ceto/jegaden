bae60df4ea7dc105e65a809aa4228962e0340833
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2020-09-23 00:45:57 +0200

    features translatable

--- templates/features.php
+++ templates/features.php
@@ -5,19 +5,19 @@
             <li>
                 <div class="featcard">
                     <svg class="icon featcard__icon"><use xlink:href="#icon-plecsni1"></use></svg>
-                    <p class="featcard__text">Outstanding experience in cardiac surgery</p>
+                    <p class="featcard__text"><?php _e('Outstanding experience in cardiac surgery','jegaden') ?></p>
                 </div>
             </li>
             <li>
                 <div class="featcard">
                     <svg class="icon featcard__icon"><use xlink:href="#icon-plecsni2"></use></svg>
-                    <p class="featcard__text">30-year experience, over than 4500 operations</p>
+                    <p class="featcard__text"><?php _e('30-year experience, over than 4500 operations','jegaden') ?></p>
                 </div>
             </li>
             <li>
                 <div class="featcard">
                     <svg class="icon featcard__icon"><use xlink:href="#icon-plecsni3"></use></svg>
-                    <p class="featcard__text">Expert in Minimally Invasive Cardiac Surgery</p>
+                    <p class="featcard__text"><?php _e('Expert in Minimally Invasive Cardiac Surgery','jegaden') ?></p>
                 </div>
             </li>
         </ul>
