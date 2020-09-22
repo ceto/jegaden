70020a9042f4c0081d0ee520d26449248f50e700
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2020-09-23 00:02:34 +0200

    hero tranlatable

--- templates/hero.php
+++ templates/hero.php
@@ -2,10 +2,10 @@
     <div class="hero__inner grid-container grid-container--narrow">
         <div class="hero__content">
             <p class="hero__subcaption">
-                Minimally Invasive and Robotic Surgery, Cardiac Valve Repair, Coronary bypass Surgery
+                <?php _e('Minimally Invasive and Robotic Surgery, Cardiac Valve Repair, Coronary bypass Surgery','jegaden') ?>
             </p>
-            <h2 class="hero__caption">"An Experience you can trust"</h2>
-            <a href="#services" class="hero__action button">Read More</a>
+            <h2 class="hero__caption">"<?php _e('An Experience you can trust','jegaden') ?>"</h2>
+            <a href="#services" class="hero__action button"><?php _e('Read More','jegaden') ?></a>
         </div>
         <figure class="hero__fig">
             <?php if ( $image = get_field('hero_image') ) : ?>
