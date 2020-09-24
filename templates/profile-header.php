84e1d654ddbcee118034f46c613178f54bf20fc9
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2020-09-24 22:56:58 +0200

    profile-header translatable

--- templates/profile-header.php
+++ templates/profile-header.php
@@ -2,8 +2,8 @@
 
 <div class="pagehead pagehead--profile">
     <div class="pagehead__content grid-container grid-container--xnarrow">
-        <p class="pagehead__subtitle">Minimally Invasive and Robotic Surgery, Cardiac Valve Repair, Coronary bypass Surgery</p>
-        <h1 class="pagehead__title">Prof. Olivier Jegaden, MD, PHD. Cardiac Surgeon Professor of Cardiac Surgery</h1>
+        <p class="pagehead__subtitle"><?php _e('Minimally Invasive and Robotic Surgery, Cardiac Valve Repair, Coronary bypass Surgery','jegaden') ?></p>
+        <h1 class="pagehead__title"><?php _e('Prof. Olivier Jegaden, MD, PHD.','jegaden') ?><?php _e('Cardiac Surgeon Professor of Cardiac Surgery','jegaden') ?></h1>
     </div>
     <!-- <figure class="pagehead__bg">
         <img src="<?= get_stylesheet_directory_uri(); ?>/assets/images/homehero.jpg" alt="">
