e2b1143e2c0eaa9de929261c3ecbc839618f743b
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2020-09-24 23:54:26 +0200

    services

--- templates/post-header.php
+++ templates/post-header.php
@@ -2,7 +2,7 @@
 
 <div class="pagehead pagehead--post">
     <div class="pagehead__content grid-container grid-container--xnarrow">
-        <p class="pagehead__subtitle"><a href="<?php the_permalink( get_option( 'page_for_posts' )); ?>">Patient Resources</a></p>
+        <p class="pagehead__subtitle"><a href="<?php the_permalink( get_option( 'page_for_posts' )); ?>"><?php _e('Patient Resources','jegaden') ?></a></p>
         <h1 class="pagehead__title"><?= Titles\title(); ?></h1>
     </div>
     <figure class="pagehead__bg">
