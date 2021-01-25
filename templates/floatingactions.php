ac7474a33880592b68ab2c0464bf3630e026033a
Gabor Szabo <szabogabi@gmail.com>
2021-01-25 15:40:28 +0100

    whatsapp + email Added

--- templates/floatingactions.php
+++ templates/floatingactions.php
@@ -1,9 +1,15 @@
 <aside class="flactions" data-magellan>
     <a href="#contact"><?php _e('Book an Appointment','jegaden') ?></a>
-    <!-- <a target="_blank" href="https://wa.me/9710581682607">
-        <svg class="icon">
+    <a target="_blank" href="https://wa.me/9710581682607">
+        <!-- <svg class="icon">
             <use xlink:href="#icon-phone"></use>
-        </svg>
-        971 (0) 5816 82607
-    </a> -->
+        </svg> -->
+        <span>WhatsApp</span>
+    </a>
+    <a href="mailto:prolivierjegaden@gmail.com">
+        <!-- <svg class="icon">
+            <use xlink:href="#icon-emailchat"></use>
+        </svg> -->
+        <span>E-mail</span>
+    </a>
 </aside>
