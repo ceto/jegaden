b4cc80cbc3798a5fc74589e8dccd5b5225b06bdd
Gábor Szabó <szabogabi@gmail.com>
2022-09-01 16:06:07 +0200

    places wher work typo

--- templates/placeswherework.php
+++ templates/placeswherework.php
@@ -1,6 +1,6 @@
 <section class="placeswherework ps ps--light ps--bordered">
     <div class="grid-container grid-container--xnarrow">
-        <h2><?php _e('Places where work','jegaden') ?></h2>
+        <h2><?php _e('Places where Dr. Jegaden works','jegaden') ?></h2>
         <ul class="placeslist">
             <li>
                 <div class="placecard">
@@ -19,7 +19,7 @@
                         <img src="<?= get_stylesheet_directory_uri(); ?>/dist/images/buildings/mohammed.jpg" alt="">
                     </figure>
                     <h3 class="placecard__title"><?php _e('Mohammed Bin Rashid University','jegaden') ?></h3>
-                    <p class="placecard__text"><?php _e('Buildin 14, - Dubai - United Arab Emirates','jegaden') ?></p>
+                    <p class="placecard__text"><?php _e('Building 14, - Dubai - United Arab Emirates','jegaden') ?></p>
                 </div>
             </li>
             <li>
